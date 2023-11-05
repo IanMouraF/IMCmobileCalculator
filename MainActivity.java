0e7ff287ec491cf4326aab3b6d2e80ccff2359b1
samuelVRcoder <118441330+samuelVRcoder@users.noreply.github.com>
2023-11-05 10:17:05 -0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -39,34 +39,34 @@ public class MainActivity extends Activity {
       
       if(imc > 0){
       
-      if(imc < 18.5 ){
-      
-        
-        tv.setText(imc.toString() + " - Abaixo do peso normal .");
-        
-      }else if((imc >= 18.5)&&(imc <= 24.9)){
-        
-        tv.setText(imc.toString() + " - Peso normal .");
-        
-        
-      }else if((imc >= 25)&&(imc <= 29.9)){
-        
-        tv.setText(imc.toString() + " - Excesso de Peso .");
-        
-        
-      }else if((imc >= 30)&&(imc <= 34.9)){
-        
-        tv.setText(imc.toString() + " - Obesidade Classe I .");
-        
-      }else if((imc >= 35.5)&&(imc <= 39.9)){
-        
-        tv.setText(imc.toString() + " - Obesidade Classe II .");
-        
-      }else if((imc >= 40)){
-        
-        tv.setText(imc.toString() + " - Obesidade Classe III.");
-        
-      }
+         if(imc < 18.5 ){
+         
+           
+           tv.setText(imc.toString() + " - Abaixo do peso normal .");
+           
+         }else if((imc >= 18.5)&&(imc <= 24.9)){
+           
+           tv.setText(imc.toString() + " - Peso normal .");
+           
+           
+         }else if((imc >= 25)&&(imc <= 29.9)){
+           
+           tv.setText(imc.toString() + " - Excesso de Peso .");
+           
+           
+         }else if((imc >= 30)&&(imc <= 34.9)){
+           
+           tv.setText(imc.toString() + " - Obesidade Classe I .");
+           
+         }else if((imc >= 35.5)&&(imc <= 39.9)){
+           
+           tv.setText(imc.toString() + " - Obesidade Classe II .");
+           
+         }else if((imc >= 40)){
+           
+           tv.setText(imc.toString() + " - Obesidade Classe III.");
+           
+         }
         
       }else{
         
